76cb458ebbb91e9ef7e627293fdef1f1e9a96bd8
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-06-06 19:35:44 +0200

    contact form creation

--- src/pages/ContactsPage.vue
+++ src/pages/ContactsPage.vue
@@ -1,6 +1,6 @@
 <script>
 export default {
-    name: "ContactsPage"
+    name: "ContactsPage",
 }
 </script>
 
@@ -8,6 +8,23 @@ export default {
 <template>
     <section>
       <h1 class="mt-5 mb-3">Contacts Page</h1>
+      <div class="container">
+        <form>
+          <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" class="form-control" id="name">
+          </div>
+          <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email">
+          </div>
+          <div class="mb-3">
+            <label for="message" class="form-label">Message</label>
+            <textarea class="form-control" id="message" rows="3"></textarea>
+          </div>
+          <button class="btn btn-primary">Submit</button>
+        </form>
+      </div>
     </section>
 </template>
 
